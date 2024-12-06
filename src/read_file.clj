2baef8e90fbe164c14de4e6970e9dbfe1e4bde4d
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-06 16:57:46 +0700

    upload read and write file.txt lesson.

---
+++ src/read_file.clj
@@ -0,0 +1,20 @@
+(ns read-file
+  (:require [clojure.java.io :refer [reader]]))
+
+
+(defn read-input
+  []
+  (with-open [rdr (reader "numbers.txt")]
+    (let [lines (line-seq rdr)
+          n (-> lines first Integer/parseInt)
+          numbers (->> lines rest (take n) (map #(Integer/parseInt %)))]
+      (spit "output.txt" "Gia tri lon nhat la ")
+      (spit "output.txt" (apply max numbers) :append true))))      
+
+(read-input)
+
+
+
+
+
+(Integer/parseInt "14")
