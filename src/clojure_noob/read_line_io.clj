fcdf475035709ad9220275e8a0d92f5191f2b3a0
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-06 14:14:58 +0700

    xoa file

--- src/clojure_noob/read_line_io.clj
+++ src/clojure_noob/read_line_io.clj
@@ -2,18 +2,20 @@
 
 (defn input
   []
-  (println "Nhap vao so phan tu: " 
-  (let [n (Integer/parseInt (read-line))]
-    (println n)
-    (loop [i 0
-           input-list []]
-      (if (< i n)
-        (do
-          (println "Nhap gia tri thu " (+ i 1) ":")
-          (let [value (Integer/parseInt (read-line))]
-            (recur (inc i) (conj input-list value))))
-        (do
-          (println "Gia tri lon nhat la: " (apply max input-list))
-          (println "Gia tri nho nhat la: " (apply min input-list))))))))
+  (println "Nhap vao so phan tu: "
+           (let [n (Integer/parseInt (read-line))]
+             (println n)
+             (loop [i 0
+                    input-list []]
+               (if (< i n)
+                 (do
+                   (println "Nhap gia tri thu " (+ i 1) ":")
+                   (let [value (Integer/parseInt (read-line))]
+                     (println value)
+                     (recur (inc i) (conj input-list value))))
+                 (do
+                   (println "Gia tri lon nhat la: " (apply max input-list))
+                   (println "Gia tri nho nhat la: " (apply min input-list))))))))
 
 (input)
+
