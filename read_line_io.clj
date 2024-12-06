22e82ab0df9e6e349460c8102ab03ae8ed46eb83
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-06 13:36:39 +0700

    add read line lesson.

---
+++ read_line_io.clj
@@ -0,0 +1,17 @@
+(ns read-line-io)
+
+(defn input
+  []
+  (let [n (Integer/parseInt (read-line))]
+    (println "Nhap vao so phan tu: " n)
+    (loop [i 0
+           input-list []]
+      (if (< i n)
+        (do
+          (println "Nhap gia tri thu " (+ i 1) ":")
+          (let [value (Integer/parseInt (read-line))]
+            (println value)
+            (recur (inc i) (conj input-list value))))
+        (do
+          (println "Gia tri lon nhat la: " (apply max input-list))
+          (println "Gia tri nho nhat la: " (apply min input-list)))))))
