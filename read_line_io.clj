977ab235737cdde796ae68723c406b283da89905
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-06 13:48:53 +0700

    Upd improve logic.

--- read_line_io.clj
+++ read_line_io.clj
@@ -2,6 +2,7 @@
 
 (defn input
   []
+  (println "Nhap vao so phan tu: " n
   (let [n (Integer/parseInt (read-line))]
     (println "Nhap vao so phan tu: " n)
     (loop [i 0
@@ -10,10 +11,9 @@
         (do
           (println "Nhap gia tri thu " (+ i 1) ":")
           (let [value (Integer/parseInt (read-line))]
-            (println value)
             (recur (inc i) (conj input-list value))))
         (do
           (println "Gia tri lon nhat la: " (apply max input-list))
-          (println "Gia tri nho nhat la: " (apply min input-list)))))))
+          (println "Gia tri nho nhat la: " (apply min input-list))))))))
 
 (input)
