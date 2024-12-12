0f21e91258038360236f0895c5dc10ba213a41fd
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-12 22:48:56 +0700

    upd bubblesort (kho qua)

--- src/clojure_noob/bubblesort.clj
+++ src/clojure_noob/bubblesort.clj
@@ -2,18 +2,36 @@
 
 
 
+(defn switch
+  [a]
+  (if (< (a 0) (a 1))
+    a
+    [(a 1) (a 0)]))
 
 
-(defn add-to-endlist
+
+(concat (switch (subvec [10 5 9 76 85 2] 0 2)) (subvec [10 5 9 76 85 2] 2))
+
+  (def ls [5 4 2 5 2])
+(defn bubble-sort
   []
-  (let [ds [1 4 5 10 6 3 -2]
-        new-list []
-        value (apply max ds)]
-    (conj new-list value)
-    (remove ds value)))
+  (loop [i0 0
+         i2 2]
+    (if (< i2 (count ls))
+      (do
+        (println (concat [] (switch (subvec ls i0 i2))))
+        (recur (inc i0) (inc i2))))))
+
+(bubble-sort)
 
 
-(add-to-endlist)
+(subvec [1 5 9 76 85 2] 0 2)
+
+
+
+
+
+
 
 
 (defn add-to-endlist
@@ -29,9 +47,16 @@
 
 
 
-(def n [])
-(let [new-list []]
-  (conj new-list (apply max ds)))
 
-(let [value (apply max ds)]
-  value)
+
+
+
+
+
+(defn swap [a] (if (> (a 1) (a 0)) a [(a 1) (a 0)]))
+(defn swap-at
+  [s ith]
+  (vec (concat (subvec s 0 ith)
+               (swap (subvec s ith (+ ith 2)))
+               (subvec s (+ ith 2)))))
+(swap-at [1 3 2] 1)
