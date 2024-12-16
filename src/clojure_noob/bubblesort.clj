d238c00fca2380fc7718ae4b1c6f54111e6a80a8
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-16 14:16:00 +0700

    debug successfully

--- src/clojure_noob/bubblesort.clj
+++ src/clojure_noob/bubblesort.clj
@@ -1,59 +1,37 @@
 (ns clojure-noob.bubblesort)
 
-
-
 (defn switch
   [a]
   (if (< (a 0) (a 1))
     a
     [(a 1) (a 0)]))
-(defn switch-at
 
+(defn switch-at
   [ls ith]
   (vec (concat (subvec ls 0 ith)
                (switch (subvec ls ith (+ ith 2)))
                (subvec ls (+ ith 2)))))
+
 (defn max-to-end
   [ls]
   (loop [ith 0
          upd ls]
     (if (<= (+ ith 2) (count ls))
-      (do
-        (switch-at upd ith)
-        (recur (inc ith) (replace upd (switch-at upd ith))))
+        (recur (inc ith) (switch-at upd ith))
       upd)))
 
-(max-to-end [10 9 8 7 9 87 6 -1000 -2000 0])
-
-(switch-at [10 9 8 7 9 87 6 -1000 -2000 0] 8)
-(count [10 9 8 7 9 87 6 -1000 -2000 0])
- 
+(max-to-end [10000 9 8 -7000 6 5 4])
 
 (defn bbsort
   [ls]
-  (loop [new-ls ls]
-    (if ()
-      new-ls
-      (do
-        ;;(max-to-end ls)
-        (recur (replace new-ls (max-to-end new-ls)))))))
+  (loop [ times 1
+         new-ls ls]
+    (if (< times (count ls))
+        (recur (inc times) (max-to-end new-ls))
+      new-ls)))
 
-
-
-(bbsort [-2 1 3 1])
-
-
-(loop [upd2 upd]
-  (if (= (upd2 0) (apply min ls))
-    upd2
-    (do
-      (bbsort upd2)
-      (recur (replace upd2 (bbsort upd2))))))
-
-
-
+(bbsort [-2 0 -2000 100 3 1])
 
 (bbsort [10 9 8 7 6])
-;; dang bi stack over flow
 
-
+(replace [1 3 5 6 8 9 6543 2] [1 3])
