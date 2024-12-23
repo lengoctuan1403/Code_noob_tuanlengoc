428596a37b25d16d81d984d066d2c686e4e7a5c5
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-23 15:06:54 +0700

    second commit print branch

--- src/clojure_noob/printname.clj
+++ src/clojure_noob/printname.clj
@@ -5,3 +5,18 @@
   (println "My name is Tuan"))
 
 (printmyname)
+
+(defn printyourname
+  [yourname]
+  (println "Your name is" yourname))
+
+(printyourname "Phong ml")
+
+(defn printrandomname
+  []
+  (println "Nhap nam sinh cua ban: ")
+  (let [year (Integer/parseInt (read-line))]
+    (println year)
+    (println "Ban" (- 2024 year) "tuoi roi")))
+
+(printrandomname)
