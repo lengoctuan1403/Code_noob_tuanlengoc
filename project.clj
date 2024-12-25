49796c6e1339c3d506d0f790eba6f77cdf85ee68
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-25 23:12:56 +0700

    fixing project.clj dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.github.seancorfield/next.jdbc "1.3.981"]
+                 [org.postgresql/postgresql "42.7.4"]]
   :plugins [[cider/cider-nrepl "0.28.5"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  [com.github.seancorfield/next.jdbc "1.3.981"]
-  [org.postgresql/postgresql "42.7.4"])
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
 
 
 
