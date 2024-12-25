8bf21672ea83bc37d6cab02f94bc426f3e12a8ee
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-25 15:35:57 +0700

    add next.jdbc, jdbc driver to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure_noob "0.1.0-SNAPSHOT"
+(defproject Code_noob_tuanlengoc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,4 +8,9 @@
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  [com.github.seancorfield/next.jdbc "1.3.981"]
+  [org.postgresql/postgresql "42.7.4"])
+
+
+
